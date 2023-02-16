250a3b9dce5c069a52a133a1cb1555563cdc0157
Damy Metzke <damy.metzke@gmail.com>
2023-02-16 12:30:33 +0100

    Write test for reading and parsing excel file

--- tests/parse/test_parser.py
+++ tests/parse/test_parser.py
@@ -1,10 +1,17 @@
-from python_pytest_examples.parse.parser import TextParser, get_parser
+from python_pytest_examples.parse.parser import ExcelParser, TextParser, get_parser
 from python_pytest_examples.report import Day, Report
 
 
 def test_text_parser_used_for_text_file():
-    result = get_parser("filename.txt")
-    assert isinstance(result, TextParser)
+    parser, mode = get_parser("filename.txt")
+    assert isinstance(parser, TextParser)
+    assert mode == "r"
+
+
+def test_excel_parser_used_for_excel_file():
+    parser, mode = get_parser("filename.xlsx")
+    assert isinstance(parser, ExcelParser)
+    assert mode == "rb"
 
 
 def test_text_parser_can_read_file():
@@ -19,6 +26,23 @@ def test_text_parser_can_read_file():
     )
 
     parser = TextParser()
+    result = parser.parse(INPUT)
+
+    assert result == EXPECTED
+
+
+def test_excel_parser_can_read_file():
+    INPUT = open("test_resources/parser_input.xlsx", "rb")
+    EXPECTED = Report(
+        days=[
+            Day(day=1, income=110, spent=220),
+            Day(day=2, income=200, spent=3333),
+            Day(day=3, income=33300, spent=400),
+            Day(day=4, income=0, spent=555),
+        ]
+    )
+
+    parser = ExcelParser()
     result = parser.parse(INPUT)
 
     assert result == EXPECTED
